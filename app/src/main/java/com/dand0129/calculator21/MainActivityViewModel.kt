fcc9e8a405408540ed0fd876fbb1878c04726087
DanD1511 <ddacunha012001@gmail.com>
2023-04-28 00:27:54 -0500

    Updates in the build of the calculator

--- app/src/main/java/com/dand0129/calculator21/MainActivityViewModel.kt
+++ app/src/main/java/com/dand0129/calculator21/MainActivityViewModel.kt
@@ -4,21 +4,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel : ViewModel() {
 
     private val _number1 = MutableLiveData<String>("")
     val number1: LiveData<String> get() = _number1
 
-    private val _number2 = MutableLiveData(0)
-    val number2: LiveData<Int> get() = _number2
+    private val _number2 = MutableLiveData("")
+    val number2: LiveData<String> get() = _number2
 
-    private val _textToDisplay = MutableLiveData<String>("1")
+    private val _storageValue = MutableLiveData(0.0)
+    val storageValue: LiveData<Double> get() = _storageValue
+
+    private val _memory = MutableLiveData(0.0)
+    val memory: LiveData<Double> get() = _memory
+
+    private val _textToDisplay = MutableLiveData<String>("")
     val textToDisplay: LiveData<String> get() = _textToDisplay
 
 
     fun getNumber(number: String) {
-        _number1.value = number
-        _textToDisplay.value = number
+        _textToDisplay.value += number
+        _number1.value += number
     }
 
 
@@ -26,5 +32,68 @@ class MainActivityViewModel: ViewModel() {
         _textToDisplay.observeForever { newValue ->
             onDisplayValueChanged.invoke(newValue)
         }
+    }
+
+    fun deleteDisplay() {
+        _textToDisplay.value = _textToDisplay.value!!.dropLast(1)
+    }
+
+    fun operation(operation: String) {
+
+        when (operation) {
+            "+" -> {
+                if (_number1.value!!.isNotEmpty()) {
+                    _storageValue.value = _storageValue.value!! + _number1.value!!.toDouble()
+                    _number1.value = ""
+                    _textToDisplay.value += "+"
+                } else {
+                    _storageValue.value = _storageValue.value!! + _memory.value!!
+                    _number1.value = ""
+                    _textToDisplay.value += "+"
+                }
+            }
+
+            "-" -> {
+                if (_number1.value!!.isNotEmpty()) {
+                    _storageValue.value = _storageValue.value!! - _number1.value!!.toDouble()
+                    _number1.value = ""
+                    _textToDisplay.value += "-"
+                } else {
+                    _storageValue.value = _storageValue.value!! - _memory.value!!
+                    _number1.value = ""
+                    _textToDisplay.value += "-"
+                }
+            }
+
+            "X" -> {
+                if (_number1.value!!.isNotEmpty()) {
+                    _storageValue.value = _number1.value!!.toDouble()
+                    _number1.value = ""
+                    _textToDisplay.value += "X"
+                } else {
+                    _storageValue.value = _storageValue.value!! * _memory.value!!
+                    _number1.value = ""
+                    _textToDisplay.value += "X"
+                }
+            }
+
+            "÷" -> {
+                if (_number1.value!!.isNotEmpty()) {
+                    _storageValue.value = _storageValue.value!! / _number1.value!!.toDouble()
+                    _number1.value = ""
+                    _textToDisplay.value += "÷"
+                } else {
+                    _storageValue.value = _storageValue.value!! / _memory.value!!
+                    _number1.value = ""
+                    _textToDisplay.value += "÷"
+                }
+            }
+        }
+    }
+
+    fun result() {
+        val result = _number1.value!!.toDouble()*_storageValue.value!!
+        _memory.value = result
+        _textToDisplay.value = result.toString()
     }
 }
