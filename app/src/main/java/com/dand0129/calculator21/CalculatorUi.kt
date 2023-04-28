390e7823fd83a05b8baadfdd3f37de65430d4402
DanD1511 <ddacunha012001@gmail.com>
2023-04-28 00:27:54 -0500

    Updates in the build of the calculator

--- app/src/main/java/com/dand0129/calculator21/CalculatorUi.kt
+++ app/src/main/java/com/dand0129/calculator21/CalculatorUi.kt
@@ -1,12 +1,16 @@
 package com.dand0129.calculator21
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class CalculatorUi: ComponentActivity() {
@@ -18,7 +22,11 @@ fun Main(
     Column {
         Display(viewModel = viewModel)
         Spacer(modifier = Modifier.height(30.dp))
-       Keyboard(viewModel = viewModel, modifier = Modifier.size(50.dp))
+        Box(
+            modifier = Modifier.aspectRatio(1f)
+        ) {
+            Keyboard(viewModel = viewModel, modifier = Modifier.size(50.dp))
+        }
     }
 }
 
--- app/src/main/java/com/dand0129/calculator21/MainActivity.kt
+++ app/src/main/java/com/dand0129/calculator21/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import com.dand0129.calculator20.NumericButtons
 import com.dand0129.calculator21.ui.theme.Calculator21Theme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         val viewModel: MainActivityViewModel by viewModels()
         setContent {
             Calculator21Theme {
-               Main(viewModel)
+                Main(viewModel)
             }
         }
     }
